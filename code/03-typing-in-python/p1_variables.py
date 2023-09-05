50e5b693c0baabfcda6e5592961f15837a519443
Michael Kennedy <michael@talkpython.fm>
2023-09-05 10:45:17 -0700

    Nullability.

--- code/03-typing-in-python/p1_variables.py
+++ code/03-typing-in-python/p1_variables.py
@@ -2,6 +2,8 @@
 #
 # syntax of "x: type" *********************
 #
+import typing
+from typing import Optional
 
 x = 7
 y: int = 10  # Explicitly declare the type as int.
@@ -31,6 +33,35 @@ print(u, v, c, text, b, truth, lst, s)
 
 lst = s  # <- Error
 
+# ##########################################
+#
+# Nullable types *********************
+#
+
+z: int = 42
+print(z)
+
+z = None  # <- Error!
+
+z2: str = "Never nothing!"
+print(z2)
+z2 = None  # <- Error!
+
+z3: typing.Optional[int] = 43
+print(z3)
+z3: Optional[int] = 43
+z4: int | None = 43
+print(z3)
+z3 = None
+print(z3)
+z3 = 44
+print(z3)
+z3 = "ABC"  # <- Error!
+
+# ##########################################
+#
+# Unions *********************
+#
 
 
 
