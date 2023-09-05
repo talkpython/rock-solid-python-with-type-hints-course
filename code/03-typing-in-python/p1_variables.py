fc53461f245a83b9ca244ea490cc092446fc7798
Michael Kennedy <michael@talkpython.fm>
2023-09-05 09:23:21 -0700

    Types and variable declarations

---
+++ code/03-typing-in-python/p1_variables.py
@@ -0,0 +1,14 @@
+# ##########################################
+#
+# syntax of "x: type" *********************
+#
+
+x = 7
+y: int = 10
+
+print(x, y)
+
+x = "Happy numbers"
+y = "Sad numbers"
+
+print(x, y)
