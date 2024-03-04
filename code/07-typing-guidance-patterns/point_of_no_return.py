5d1899183ad0aaadf659993fb07aac3f0166254a
Michael Kennedy <michael@talkpython.fm>
2024-03-04 13:56:10 -0800

    No return

--- code/07-typing-guidance-patterns/point_of_no_return.py
+++ code/07-typing-guidance-patterns/point_of_no_return.py
@@ -1,10 +1,14 @@
+from typing import NoReturn
+
+
 def main():
     print("Fake web framework")
 
     try:
         text = input("Do you want to redirect or see the page? [y/n] ")
         if text == 'y':
-            redirect_to_v1('https://talkpython.fm')
+            redirect_to_v2('https://talkpython.fm')
+            # Never ran, warning!!! 
             print("We are redirecting the user...")
 
         print("You see the page content and are amazed!")
@@ -25,6 +29,19 @@ class HTTPMovedPermanently(HTTPException): ...
 
 
 # endregion
+
+# def func() -> None:
+#     print('hi')
+
+def redirect_to_v2(url, permanent=False) -> NoReturn:  # AlwaysException
+    if not url:
+        raise ValueError('url')
+
+    if permanent:
+        raise HTTPMovedPermanently(url)
+
+    raise HTTPFound(url)
+
 
 def redirect_to_v1(url, permanent=False) -> None:
     if not url:
